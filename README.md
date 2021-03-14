333ca1b6e472e35d4b74f17e684cc256de0e6523
xuyanfeng <xu_yanfeng@126.com>
2021-03-14 14:52:13 +0800

    no message

---
+++ README.md
@@ -0,0 +1,2 @@
+v2.x 为creator2.x的版本
+v3.x 为creator3.x的版本
--- extensions/creator-helper/@types/packages/shortcuts/shortcut.d.ts
+++
@@ -1,9 +0,0 @@
-export interface ShortcutItem {
-    when: string;
-    message: string;
-    shortcut: string;
-    pkgName: string;
-    rawShortcut?: string;
-}
-
-export type IShortcutItemMap = Record<string, ShortcutItem>;
